6c30306e08f7ec93d4824275ba0a9deddde39522
Rahul Kumar <rahul.ctae94@gmail.com>
2022-02-12 11:51:06 +0530

    Project Started
    Project Started

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+	<head>	
+		<title>ExaMocks - Best RSMSSB Computer instructor(Anudeshak) Prepration Website</title>
+		<meta charset="utf-8" />
+		 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		 
+	</head>
+	<body>
+	
+	<div class="navbar-fixed">
+	<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>
+		<ul id="dropdown2" class="dropdown-content">
+    <li><a href="#!">one<span class="badge">1</span></a></li>
+    <li><a href="#!">two<span class="new badge">1</span></a></li>
+    <li><a href="#!">three</a></li>
+  </ul>
+  <a class="btn dropdown-trigger" href="#!" data-target="dropdown2">Dropdown<i class="material-icons right">arrow_drop_down</i></a>
+		<a href="sass.html">MCQ</a>
+		
+		
+		</li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  </div>
+
+   <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">three</a></li>
+    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+  </ul>
+
+  <div id="test1" class="col s12">Test 1</div>
+  <div id="test2" class="col s12">Test 2</div>
+  <div id="test3" class="col s12">Test 3</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  <div id="test4" class="col s12">Test 4</div>
+  
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<script>
+	document.addEventListener('DOMContentLoaded', function() {
+		var elems = document.querySelectorAll('.dropdown-trigger');
+		var instances = M.Dropdown.init(elems, options);
+	  });
+	</script>
+	
+	</body>
+<html>
